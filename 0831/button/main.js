567ad849e2f8ad6bfc60e8d107f6c4dc72370e4b
doujunchen <765709671@qq.com>
2016-09-01 00:40:56 +0800

    button

---
+++ 0831/button/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2016/8/31 0031.
+ */
+$(function () {
+    $("#container").accordion({collapsible:true}).buttonset();
+    // $("#container2").accordion({collapsible:true});
+    // $("#a").button("click",function () {
+    //     $("#container2").accordion({cative:1});
+    // });
+    // $("#b").button(function () {
+    //
+    // })
+    // $("#container").;
+
+});
